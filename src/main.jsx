ac216a1eac4620db4f6a276782dbee2bcc427b76
stoptacoman <walsh.dc@gmail.com>
2022-04-23 16:31:47 -0400

    Installed MUI and project fonts

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "modern-normalize";
-import "./index.css";
+// import "modern-normalize";
+// import "./index.css";
+import "@fontsource/cabin";
+import "@fontsource/source-code-pro";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
